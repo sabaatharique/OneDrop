d5cc6019f9f6f0177c946293f321af8b15ac5a26
sabaatharique <saba.5.9.2003@gmail.com>
2025-08-18 19:11:03 +0600

    initial commit

---
+++ src/main/java/com/LesMiserables/OneDrop/OneDropApplication.java
@@ -0,0 +1,13 @@
+package com.LesMiserables.OneDrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.LesMiserables.OneDrop.OneDropApplication.class, args);
+	}
+
+}
